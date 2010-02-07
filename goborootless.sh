707e719d132ec34e469acece1f9fbddca73a42d9
Daed Lee <daed@thoughtsofcode.com>
2010-02-06 22:41:03 -0500

    Corrected licensing grammar and formatting.

--- goborootless.sh
+++ goborootless.sh
@@ -5,8 +5,8 @@
 # Licensed under the New BSD License (the "License").
 # You may not use this file except in compliance with the License.
 #
-# You should have received a copy of the License along with this program. If not,
-# contact the copyright holder.
+# You should have received a copy of the License along with this program.
+# If not, contact the copyright holder(s).
 
 
 #########################
